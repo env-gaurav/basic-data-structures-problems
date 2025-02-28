ea72879e777d68c1605e31e5d62abf3410ee4db2
env-gaurav <gaurav2528singh@gmail.com>
2025-02-28 23:47:12 +0530

    sum of digits

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 # Reverse the digits
-num = 12345
-result = 0
-while num > 0:
-    r = num%10
-    result = result*10+r
-    num = num//10
+def reverseTheDigit(num):
+    result = 0
+    while num > 0:
+        r = num%10
+        result = result*10+r
+        num = num//10
+    return result
 
-print(result)
+# Addition of digits
+def addition(num):
+    result = 0
+    while num > 0:
+        r = num%10
+        result = result + r
+        num = num//10
+    return result
+
+num = 12345
+print("Reversed Digit:", reverseTheDigit(num))
+print("Sum of Digits:", addition(num))
