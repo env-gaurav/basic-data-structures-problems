e73ef1d12eee7c0cc972438f3453da7362a51990
env-gaurav <gaurav2528singh@gmail.com>
2025-03-01 17:28:20 +0530

    solved basic data structure questions

--- main.py
+++ main.py
@@ -40,8 +40,58 @@ def countDigits(num):
         num = num//10
     return result
 
+def largestDigit(num):
+    largest = 0
+    while num>0:
+        r = num%10
+        if r>largest:
+            largest = r
+        num = num//10
+    return largest
+
+def firstAndLastDigit(num):
+    ld= num%10
+    while num>=10:
+        num = num//10
+    fd = num
+    return fd,ld
+
+def armstrong(num):
+    cp = num
+    result= 0
+    power = countDigits(num)
+    while num>0:
+        r= num%10
+        result = (r**power)+ result
+        num = num//10
+    return result == cp
+
+def isArmstrong(num):
+    check = armstrong(num)
+    if check == True:
+        return f"{num} is an Armstrong number"
+    else:
+        return f"{num} is not an Armstrong number"
+
+def countOddEven(num):
+    even = 0
+    odd = 0
+    while num>0:
+        r = num%10
+        if r%2 == 0:
+            even= even+1
+        else:
+            odd = odd+1
+        num=num//10
+    return even,odd
+
 
 num = 12345
+
+print(countOddEven(num))
+print(isArmstrong(153))
+print("First and Last Digit:",firstAndLastDigit(num))
+print("Largest Digit:", largestDigit(num))
 print("Reversed Digit:", reverseTheDigit(num))
 print("Sum of Digits:", additionOfDigits(num))
 print("product Of Digits:", productOfDigits(num))
